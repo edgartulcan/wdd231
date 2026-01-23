a115c360f1f4d074ff1bb9dbc88ab4d9fbbcf75b
edgartulcan <etulcnlvarez@byupathway.edu>
2026-01-22 20:30:27 -0500

    save

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,13 +1,13 @@
-const gridButton = document.querySelector("#grid");
-const listButton = document.querySelector("#list");
-const cards = document.querySelector("cards");
+// const gridButton = document.querySelector("#grid");
+// const listButton = document.querySelector("#list");
+// const cards = document.querySelector("cards");
 
-gridButton.addEventListener("click", () => {
-     cards.classList.add("grid");
-     cards.classList.remove("list");
- });
+// gridButton.addEventListener("click", () => {
+//     cards.classList.add("grid");
+//     cards.classList.remove("list");
+// });
 
- listButton.addEventListener("click", () => {
-     cards.classList.add("list");
-     cards.classList.remove("grid");
- });
+// listButton.addEventListener("click", () => {
+//     cards.classList.add("list");
+//     cards.classList.remove("grid");
+// });
