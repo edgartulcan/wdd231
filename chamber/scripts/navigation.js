3309f201100ec6b2d850d32f3c8dad5b70a219a0
edgartulcan <etulcnlvarez@byupathway.edu>
2026-01-22 20:21:21 -0500

    save

--- chamber/scripts/navigation.js
+++ chamber/scripts/navigation.js
@@ -1,7 +1,8 @@
+const navButton = document.querySelector('#nav-button');
+const navBar = document.querySelector('#nav-bar');
 
-const menuToggle = document.getElementById('menu-toggle');
-const navMenu = document.getElementById('nav-menu');
+navButton.addEventListener('click', () => {
+    navButton.classList.toggle('show');
+    navBar.classList.toggle('show');
 
-menuToggle.addEventListener('click', () => {
-    navMenu.style.display = navMenu.style.display === 'flex' ? 'none' : 'flex';
 });
